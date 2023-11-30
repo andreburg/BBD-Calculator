81c979e2eff730dda735640322dea9f88f060b6d
Andre <47895843+andreburg@users.noreply.github.com>
2023-11-30 16:39:31 +0200

    Add Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import MathExpression from "./js/Calculator/MathExpression";
+
+const app = document.querySelector("#app");
+let expression = "((5*5+3)^2)/2+4*9";
+
+expression = new MathExpression(expression);
+const e = {
+    le: e,
+    re: e,
+    operation: () => {
+
+    },
+    getVal: () => operation(le.getVal(), re.getVal())
+}
+
+console.log(makeExpression(expression));
---
+++ js/Calculator/MathOperation.js
@@ -0,0 +1,9 @@
+export default class MathOperation {
+    constructor(operator){
+
+    }
+
+    Execute(){
+        
+    }
+}
